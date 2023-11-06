665314ed4afb699a1d8fb08f727916de2be99108
Chen, Xiaofeng [GTSCN NON-J&J] <xiaofengc@ciandt.com>
2023-11-06 21:00:31 +0800

    write to xlsx

--- dist/index.js
+++ dist/index.js
@@ -18,24 +18,41 @@ const pdf_js_extract_1 = require("pdf.js-extract");
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const figlet_1 = __importDefault(require("figlet"));
+const ExcelJS = require('exceljs');
 const program = new commander_1.Command();
 program
     .version('0.0.1')
     .description('A CLI link extracter that can be used to extract links from any pdf')
-    .option('-f, --file  [value]', 'Extract links from a pdf')
+    .option('-f, --from  <value>', 'Extract links from a pdf')
+    .option('-t, --to [value]', 'Write links to a excel file')
     .parse(process.argv);
 const handleExtract = (file) => __awaiter(void 0, void 0, void 0, function* () {
     const pdfExtract = new pdf_js_extract_1.PDFExtract();
     const options = {};
     const data = yield pdfExtract.extract(file, options);
     const links = data.pages.map((page) => page.links).flat();
-    console.log(links);
+    return links;
 });
 const options = program.opts();
-if (options.file) {
-    const file = path_1.default.resolve(process.cwd(), options.file);
+if (options.from) {
+    const file = path_1.default.resolve(process.cwd(), options.from);
     if (fs_1.default.existsSync(file)) {
-        handleExtract(file);
+        if (options.to) {
+            const workbook = new ExcelJS.Workbook();
+            const worksheet = workbook.addWorksheet('Links');
+            worksheet.columns = [{ header: 'Link', key: 'link' }];
+            handleExtract(file).then((links) => {
+                links.forEach((link) => {
+                    worksheet.addRow([link]);
+                });
+                workbook.xlsx.writeFile(options.to);
+            });
+        }
+        else {
+            handleExtract(file).then((links) => {
+                console.log(links);
+            });
+        }
     }
     else {
         console.log('File does not exist');
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@
 
 import { Command } from 'commander'
 import { PDFExtract, PDFExtractOptions } from 'pdf.js-extract'
-import fs from 'fs'
+import fs, { link } from 'fs'
 import path from 'path'
 import figlet from 'figlet'
 
+const ExcelJS = require('exceljs')
 const program = new Command()
 program
   .version('0.0.1')
   .description(
     'A CLI link extracter that can be used to extract links from any pdf'
   )
-  .option('-from, --from  [value]', 'Extract links from a pdf')
-  .option('-to, --to [value]', 'Write links to a excel file')
+  .option('-f, --from  <value>', 'Extract links from a pdf')
+  .option('-t, --to [value]', 'Write links to a excel file')
   .parse(process.argv)
 
 const handleExtract = async (file: string) => {
@@ -21,14 +22,29 @@ const handleExtract = async (file: string) => {
   const options: PDFExtractOptions = {}
   const data = await pdfExtract.extract(file, options)
   const links = data.pages.map((page) => page.links).flat()
-  console.log(links)
+  return links
 }
 
 const options = program.opts()
-if (options.file) {
-  const file = path.resolve(process.cwd(), options.file)
+
+if (options.from) {
+  const file = path.resolve(process.cwd(), options.from)
   if (fs.existsSync(file)) {
-    handleExtract(file)
+    if (options.to) {
+      const workbook = new ExcelJS.Workbook()
+      const worksheet = workbook.addWorksheet('Links')
+      worksheet.columns = [{ header: 'Link', key: 'link' }]
+      handleExtract(file).then((links) => {
+        links.forEach((link) => {
+          worksheet.addRow([link])
+        })
+        workbook.xlsx.writeFile(options.to)
+      })
+    } else {
+      handleExtract(file).then((links) => {
+        console.log(links)
+      })
+    }
   } else {
     console.log('File does not exist')
   }
